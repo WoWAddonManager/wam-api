51bd9c55c97db728f700ba807e8e069e805aefa9
Jordan Ayvazian <jaayvazian@gmail.com>
2020-05-27 01:38:58 -0700

    Only update token on login, and permit more params in the url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
-require 'devise_token_auth'
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  skip_after_action :update_auth_header, raise: false, except: %i[create]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :name, :image, :registration])
 
   end
   respond_to :json
