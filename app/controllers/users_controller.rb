b6cea1284dac040e475b2c06b1c0ccb11525c96e
Jordan Ayvazian <jaayvazian@gmail.com>
2020-05-27 01:40:29 -0700

    Use devise token auth and include DeviseTokenAuth SetUserByToken concern

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,15 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: %i[show]
-  # before_action :user_params, only: %i[:show]
 
+  # before_action :user_params, only: %i[:show]
+  DeviseTokenAuth::Concerns::SetUserByToken
 
   def show
     render json: User.find_by({id: params[:id]})
   end
 
-
   private
 
-  def authenticate_user!
-    @user = User.find_by({id: params[:id]})
-    if @user.tokens.has_key?(params[:token])
-      render json: @user
-    else
-
-    end
-  end
 
   def user_params
     params.require(:user).permit(:name, :addons, :id, :uid, :tokens)
