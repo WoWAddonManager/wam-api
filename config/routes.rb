4df64f6adc173055f11df9b434f389fb2a0a978b
Jordan Ayvazian <jaayvazian@gmail.com>
2020-05-27 01:40:57 -0700

    Add route to get a user by id

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-  # devise_for :users
+
   scope '/api' do
     resources :addons
+    get "users/:id", to: "users#show"
   end
+
 end
 
-
-# TODO: OAuth from the client to get a token and use that token for when the client request for data
